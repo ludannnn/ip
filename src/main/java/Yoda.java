196aed7b272b049a959bf9ce41def75064eae3b7
ludannnn <nathanlikesegg@gmail.com>
2025-08-20 13:32:38 +0800

    level-1

--- src/main/java/Yoda.java
+++ src/main/java/Yoda.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
+
 public class Yoda {
     public static void main(String[] args) {
+        final String name = "Yoda";
+
         System.out.println("""
-                _________________________
-                Hi I'm your chatbot Yoda
+                _______________________
+                Hi i am Yoda
+                _______________________
                 What can i do for you?
-                _________________________
-                ByeBye
-                _________________________
-                """
-        );
+                """);
+
+        Scanner scanner = new Scanner(System.in);
+        boolean isRunning = true;
+        String input;
+
+        while (isRunning) {
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                isRunning = false;
+                break;
+            } else {
+                System.out.println("_______________________");
+                System.out.println(input);
+            }
+        }
+
+
+        System.out.println("Bye. See you again!");
+        scanner.close();
     }
 }
